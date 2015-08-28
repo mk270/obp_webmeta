6390097311ecc03f92e949e1a4838f9380b3a32d
Martin Keegan <martin@no.ucant.org>
2015-08-28 17:01:42 +0100

    wire things up correctly to the meta tag

--- render.js
+++ render.js
@@ -5,20 +5,12 @@
 
 
 var get_short_doi = function() {
-    return $("#description span:contains(\"DOI:\")").next().text();
-};
-
-var set_dimensions = function(dim_text) {
-    $("#description").append( 
-        $("<div><span style=\"font-weight: bold\">Dimensions: </span>" 
-          + dim_text + "</div>"
-         ) 
-    );
+    return $("meta[scheme=DOI][name=DC.identifier]").first().attr("content");
 };
 
 var state_online_readership = function(total) {
-    var div = $("<div>Total online readers: " + total + "</div>");
-    $("#report_total_readers").append(div);
+    var div = "<div>Total online readers: " + total + "</div>";
+    $("#description").append(div);
 };
 
 var lookup_dimensions = function(doi) {
@@ -27,14 +19,13 @@ var lookup_dimensions = function(doi) {
         url: url,
         dataType: "json"
     }).done(function(results) {
-        console.log(results);
-        set_dimensions(results.dimensions);
         state_online_readership(results.total_online_readers);
     })
 };
 
 $(document).ready(function() {
     var doi = get_short_doi();
-    console.log("DOI" : doi);
-    lookup_dimensions(doi);
+    if(doi !== "") {
+        lookup_dimensions(doi);
+    }
 });
