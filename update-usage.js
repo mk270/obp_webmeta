5b87fcc1b979ba868c991b84d15b654d151916c0
Martin Keegan <martin@no.ucant.org>
2015-09-01 15:43:48 +0100

    add doc

--- update-usage.js
+++ update-usage.js
@@ -1,5 +1,14 @@
 // A quick tool to update the usage stats for books on the OBP website
 
+/** The DOI is provided in a <meta> tag in the <head> thus:
+
+       <meta name="DC.identifier" scheme="DOI"
+             content="http://dx.doi.org/10.11647/OBP.0060"/>
+
+    We retrieve this and use it in an AJAX request, whose results we
+    display in the page
+*/
+
 (function() {
   var get_short_doi = function() {
     return $("meta[scheme=DOI][name=DC.identifier]").first().attr("content");
